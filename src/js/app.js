0fa48ff27e22f9020dcb0025ff5818674cd27dc4
Ludmila <lvminzulina@mail.ru>
2023-06-18 15:14:51 +0300

    2

--- src/js/app.js
+++ src/js/app.js
@@ -3,10 +3,13 @@ const popover = document.querySelector('[data-widget="popover-top"]');
 
 btn.addEventListener('click', (e) => {
   e.preventDefault();
+  const positionBth = btn.getBoundingClientRect();
+  popover.style = `position:absolute; left:${positionBth.left-670}px; top:${positionBth.top-310}px`;
 
   if (popover.classList.contains('hidden')) {
     popover.classList.remove('hidden');
     popover.classList.add('active');
+    
   } else {
     popover.classList.remove('active');
     popover.classList.add('hidden');
